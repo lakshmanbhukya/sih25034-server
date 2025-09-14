e257544bfc603142693d93cae4d224ea64e4f5ee
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-09-14 15:08:13 +0530

    feat : cors setup for deployment

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
